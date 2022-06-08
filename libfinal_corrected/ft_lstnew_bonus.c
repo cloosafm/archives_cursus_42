2ea2db4e02064e8237b46062c5f8ba8da2467e4a
cloosafm <alexandra.cloos@gmail.com>
2022-06-08 09:12:48 +0200

    libft, version 112% et version ready pour 125

---
+++ libfinal_corrected/ft_lstnew_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acloos <acloos@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/27 14:56:02 by acloos            #+#    #+#             */
+/*   Updated: 2022/06/07 11:58:01 by acloos           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*newlist;
+
+	newlist = (t_list *)malloc(sizeof(t_list));
+	if (newlist == NULL)
+		return (NULL);
+	newlist->content = content;
+	newlist->next = NULL;
+	return (newlist);
+}
---
+++ libfinal_corrected/ft_strchr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acloos <acloos@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 09:36:03 by acloos            #+#    #+#             */
+/*   Updated: 2022/06/03 09:47:05 by acloos           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (unsigned char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+/*
+int	main()
+{
+   char a[50] = "bonjour";
+
+   printf("%s\n", ft_strchr(a, '\0'));
+   printf("%s\n", strchr(a, '\0'));
+
+   return 0;
+}
+*/
